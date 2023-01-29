589c74e6682f7146eb7b6e37121f559e02496270
Valeriy95 <113552340+Valeriy95@users.noreply.github.com>
2023-01-29 20:48:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -260,10 +260,11 @@ function durat () {
    let time = `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
    console.log(time);
    if(playList[playNum].duration === time) {
-     isPlay = false;
+//      isPlay = false;
      sec = 0;
      min = 0;
      playNext();
+     playBtn.classList.remove('pause');
    }
    setTimeout(durat, 1000);
 };
