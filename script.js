325413f0cdcde65b929cacf6d5e023d9894d79e6
Valeriy95 <vmariukhnyk@gmail.com>
2023-01-29 13:07:27 +0400

    feat: added test

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+async function getQuotes() {  
+   const quotes = 'data.json';
+   const res = await fetch(quotes);
+   const data = await res.json(); 
+   let a = Math.floor(Math.random() * 3);
+   console.log(a);
+   console.log(data[a]);
+ }
+ getQuotes();
