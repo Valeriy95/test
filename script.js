078e8191591093eeddcdbbfd6830954655b91e22
Valeriy95 <113552340+Valeriy95@users.noreply.github.com>
2023-02-13 17:49:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -647,30 +647,44 @@ changeHideBlocs.forEach(changeHideBlocs => changeHideBlocs.addEventListener('cha
    if(changeHideBlocs.value == 'time') {
       const time = document.querySelector('.time');
       time.classList.toggle('opacity-bloc');
+      localStorage.setItem('changeHideBlocs', changeHideBlocs.value);
    }
    if(changeHideBlocs.value == 'date') {
       const date = document.querySelector('.date');
       date.classList.toggle('opacity-bloc');
+      localStorage.setItem('changeHideBlocs', changeHideBlocs.value);
    };
    if(changeHideBlocs.value == 'greeting') {
       const greeting = document.querySelector('.greeting-container');
       greeting.classList.toggle('opacity-bloc');
+      localStorage.setItem('changeHideBlocs', changeHideBlocs.value);
    };
    if(changeHideBlocs.value == 'quote') {
       const quote = document.querySelector('.quote');
       const author = document.querySelector('.author');
       quote.classList.toggle('opacity-bloc');
       author.classList.toggle('opacity-bloc');
+      localStorage.setItem('changeHideBlocs', changeHideBlocs.value);
    };
    if(changeHideBlocs.value == 'weather') {
       const weather = document.querySelector('.weather');
       weather.classList.toggle('opacity-bloc');
+      localStorage.setItem('changeHideBlocs', changeHideBlocs.value);
    };
    if(changeHideBlocs.value == 'audio') {
       const player = document.querySelector('.player');
       const wrapperPlayer = document.querySelector('.wrapper-player');
       player.classList.toggle('opacity-bloc');
       wrapperPlayer.classList.toggle('opacity-bloc');
+      localStorage.setItem('changeHideBlocs', changeHideBlocs.value);
    };
    
 }));
+
+function getChangeHideBlocs() {
+   if(localStorage.getItem('changeHideBlocs')) {
+     changeHideBlocs.value = localStorage.getItem('changeHideBlocs');
+     getLinkToImageUnsplash(changeHideBlocs.value);
+   }
+ };
+window.addEventListener('load', getChangeHideBlocs);
