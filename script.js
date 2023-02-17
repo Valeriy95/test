5c896000a9ba2936b62fec2c3ab088565cd6f776
Valeriy95 <113552340+Valeriy95@users.noreply.github.com>
2023-02-17 21:06:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function showList(btn) {
          if(btn == value.status) {
             li += `<li class="item ">
                      <label for="${index}">
-                        <input onclick=" function () { alert('hello!')}" type="checkbox" id="${index}" ${isDone}>
+                        <input onclick=" function sss () { alert('hello!')}" type="checkbox" id="${index}" ${isDone}>
                         <p class="${isDone}">${value.name}</p>
                      </label>
                      <div class="settings-todo">
